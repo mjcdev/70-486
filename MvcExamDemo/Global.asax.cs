8b27d9cedee114bd29b724534eae4bb7ffdcd6ed
Mike Childs <mchilds88@googlemail.com>
2015-10-20 22:07:40 +0100

    Display Modes
    Added Display Modes in for:
    - Mobile, built in to MVC;
    - iPhone, custom;
    - iPad, custom.

--- MvcExamDemo/Global.asax.cs
+++ MvcExamDemo/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.WebPages;
 
 namespace MvcExamDemo
 {
@@ -16,6 +17,16 @@ namespace MvcExamDemo
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            DisplayModeProvider.Instance.Modes.Insert(0, new DefaultDisplayMode("iPhone")
+            {
+                ContextCondition = (ctx => ctx.GetOverriddenUserAgent().IndexOf("iPhone", StringComparison.OrdinalIgnoreCase) >= 0)
+            });
+
+            DisplayModeProvider.Instance.Modes.Insert(1, new DefaultDisplayMode("iPad")
+            {
+                ContextCondition = (ctx => ctx.GetOverriddenUserAgent().IndexOf("iPad", StringComparison.OrdinalIgnoreCase) >= 0)
+            });
         }
     }
 }
